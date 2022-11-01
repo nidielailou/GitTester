81d78264e6e9bc35781487b70dcbccc5f5f762c0
Shuozi <947943153@qq.com>
2022-11-01 22:19:54 +0800

    回归到最开始hot-fix

--- src/main/java/shuozi/GitTest.java
+++ src/main/java/shuozi/GitTest.java
@@ -3,11 +3,6 @@ package shuozi;
 public class GitTest
 {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println("我是hot-fix");
-        System.out.println("hot-fix   Test");
-        System.out.println("master 不干了  又添加了一个test");
+        System.out.println("hot-fix也回归到了最开始");
     }
 }
