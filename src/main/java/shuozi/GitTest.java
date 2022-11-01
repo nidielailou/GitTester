91076ae998890940a8155034aaf517dbf5ad54c8
Shuozi <947943153@qq.com>
2022-11-01 22:18:17 +0800

    回归到最开始

--- src/main/java/shuozi/GitTest.java
+++ src/main/java/shuozi/GitTest.java
@@ -4,11 +4,5 @@ public class GitTest
 {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println("我是hot-fix");
-        System.out.println("master test");
-        System.out.println("hot-fix   Test");
-        System.out.println("master 不干了  又添加了一个test");
     }
 }
